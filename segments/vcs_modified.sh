tacahiroy/tmux-powerline

commit 80a79c1e56357ca7b8388bdddfe7707b018ad3d6

Improve SVN support
 - show which branch we are on if we are not in the working root
   directory
 - add svn functionality to the vcs_modified segment:
   displays the number of conflicts and the number of modified files,
   if there are any
 - add a vcs_revision segment that displays that for the CWD

segments/vcs_modified.sh
@@ -18,8 +18,8 @@ run_segment() {
 	elif [ -n "${hg_stats=$(__parse_hg_stats)}" ]; then
     	stats="$hg_stats"
 	fi
-	if [[ -n "$stats" && $stats -gt 0 ]]; then
-    	echo "${mod_symbol}${stats}"
+	if [[ -n "$stats" ]]; then
+    	echo "${stats}"
 	fi
 }
 
@@ -37,16 +37,34 @@ __parse_git_stats(){
     echo $staged
 }
 __parse_hg_stats(){
-	type svn >/dev/null 2>&1
-	if [ "$?" -ne 0 ]; then
-		return
-	fi
-    # not yet implemented
-}
-__parse_svn_stats(){
 	type hg >/dev/null 2>&1
 	if [ "$?" -ne 0 ]; then
 		return
 	fi
     # not yet implemented
+}
+__parse_svn_stats() {
+	type svn >/dev/null 2>&1
+	if [ "$?" -ne 0 ]; then
+		return
+	fi
+
+	local svn_info=$(svn info 2>/dev/null)
+	if [ -z "${svn_info}" ]; then
+		return
+	fi
+
+	local svn_wcroot=$(echo "${svn_info}" | sed -ne 's#^Working Copy Root Path: ##p')
+	local svn_st=$(cd "${svn_wcroot}"; svn st)
+	local modified=$(echo "${svn_st}" | egrep '^M' | wc -l)
+	local conflicted=$(echo "${svn_st}" | egrep '^!?\s*C' | wc -l)
+
+	#print
+	if [[ $modified -gt 0 ]] ; then
+		local ret="#[fg=colour${TMUX_POWERLINE_CUR_SEGMENT_FG}]±${modified}"
+	fi
+	if [[ $conflicted -gt 0 ]] ; then
+		local ret="#[fg=colour${svn_colour}]ϟ${conflicted} ${ret}"
+	fi
+	echo "${ret}"
 }

segments/vcs_branch.sh
@@ -61,15 +61,16 @@ __parse_svn_branch() {
 		return
 	fi
 
-	if [ ! -d ".svn/" ]; then
+	local svn_info=$(svn info 2>/dev/null)
+	if [ -z "${svn_info}" ]; then
 		return
 	fi
 
 
-	local svn_root=$(svn info 2>/dev/null | sed -ne 's#^Repository Root: ##p')
-	local svn_url=$(svn info 2>/dev/null | sed -ne 's#^URL: ##p')
+	local svn_root=$(echo "${svn_info}" | sed -ne 's#^Repository Root: ##p')
+	local svn_url=$(echo "${svn_info}" | sed -ne 's#^URL: ##p')
 
-	local branch=$(echo $svn_url | sed -e 's#^'"${svn_root}"'##g' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$' | awk '{print $1}')
+	local branch=$(echo "${svn_url}" | sed -e 's#^'"${svn_root}"'##g' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$' | awk '{print $1}')
 	echo  "#[fg=colour${svn_colour}]${branch_symbol} #[fg=colour${TMUX_POWERLINE_CUR_SEGMENT_FG}]${branch}"
 }
 

segments/vcs_compare.sh
@@ -52,17 +52,17 @@ __parse_git_stats() {
 }
 
 __parse_hg_stats() {
-	type svn >/dev/null 2>&1
-	if [ "$?" -ne 0 ]; then
-		return
-	fi
-    # not yet implemented
-}
-
-__parse_svn_stats() {
 	type hg >/dev/null 2>&1
 	if [ "$?" -ne 0 ]; then
 		return
 	fi
-    # not yet implemented
+	# not yet implemented
+}
+
+__parse_svn_stats() {
+	type svn >/dev/null 2>&1
+	if [ "$?" -ne 0 ]; then
+		return
+	fi
+	# not yet implemented
 }

segments/vcs_revision.sh
@@ -1,0 +1,43 @@
+# This prints the vcs revision in the working directory
+# currently only used in SVN
+
+# Source lib to get the function get_tmux_pwd
+source "${TMUX_POWERLINE_DIR_HOME}/lib/tmux_adapter.sh"
+
+run_segment() {
+	tmux_path=$(get_tmux_cwd)
+	cd "$tmux_path"
+
+	stats=""
+	if [[ -n "${svn_stats=$(__parse_svn_stats)}" ]]; then
+    	stats="$svn_stats"
+	elif [[ -n "${hg_stats=$(__parse_hg_stats)}" ]]; then
+    	stats="$hg_stats"
+	fi
+	if [[ -n "$stats" ]]; then
+    	echo "${stats}"
+	fi
+}
+
+__parse_hg_stats(){
+	type hg >/dev/null 2>&1
+	if [ "$?" -ne 0 ]; then
+		return
+	fi
+	# not yet implemented
+}
+__parse_svn_stats(){
+	type svn >/dev/null 2>&1
+	if [ "$?" -ne 0 ]; then
+		return
+	fi
+
+	local svn_info=$(svn info 2>/dev/null)
+	if [ -z "${svn_info}" ]; then
+		return
+	fi
+
+	local svn_ref=$(echo "${svn_info}" | sed -ne 's#^Revision: ##p')
+
+	echo "r${svn_ref}"
+}

commit 366be145fbd0e44b085cded7fea4edd5ba807a81

Fixed if statement that was broken in 80a79c1e56.

segments/vcs_modified.sh
@@ -18,8 +18,8 @@ run_segment() {
 	elif [ -n "${hg_stats=$(__parse_hg_stats)}" ]; then
     	stats="$hg_stats"
 	fi
-	if [[ -n "$stats" ]]; then
-    	echo "${stats}"
+	if [[ -n "$stats" && $stats -gt 0 ]]; then
+		echo "${mod_symbol}${stats}"
 	fi
 }
 