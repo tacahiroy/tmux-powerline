3e3ccc735a33a2624e70669703511641a06eb42e
tacahiroy <tacahiroy@gmail.com>
2015-03-05 13:20:13 +0900

    Change colours

--- themes/tacahiroy.sh
+++ themes/tacahiroy.sh
@@ -1,15 +1,15 @@
 # tacahiroy Theme
 
 if patched_font_in_use; then
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="⮂"
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="⮃"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="⮀"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="⮁"
+  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="⮂"
+  TMUX_POWERLINE_SEPARATOR_LEFT_THIN="⮃"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="⮀"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="⮁"
 else
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
+  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
+  TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
 TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'252'}
@@ -19,39 +19,40 @@ TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPA
 TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 022 255" \
-		# "hostname 33 0" \
-		# "ifstat 30 255" \
-		# "ifstat_sys 30 255" \
-		# "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		# "wan_ip 24 255" \
-		# "vcs_branch 29 88" \
-		# "vcs_compare 60 255" \
-		# "vcs_staged 64 255" \
-		# "vcs_modified 9 255" \
-		# "vcs_others 245 0" \
-	)
+  TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
+  "tmux_session_info 022 255" \
+    # "hostname 33 0" \
+    # "ifstat 30 255" \
+    # "ifstat_sys 30 255" \
+    # "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+    # "wan_ip 24 255" \
+    # "vcs_branch 29 88" \
+    # "vcs_compare 60 255" \
+    # "vcs_staged 64 255" \
+    # "vcs_modified 9 255" \
+    # "vcs_others 245 0" \
+)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		"wan_ip 24 255" \
-		"lan_ip 153 0" \
-		# "battery 166 0" \
-		# "battery 122 0" \
-		# "weather 137 255" \
-		"time 24 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
-		# "pwd 89 211" \
-		# "mailcount 9 255" \
-		# "now_playing 234 37" \
-		# "cpu 240 136" \
-		# "load 237 167" \
-		# "tmux_mem_cpu_load 234 136" \
-		#"xkb_layout 125 117" \
-		# "date_day 235 136" \
-		# "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-	)
+  TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+  # "wan_ip 24 255" \
+    "lan_ip 25 15" \
+    # "web_proxy 25 15" \
+    # "battery 166 0" \
+    # "battery 122 0" \
+    # "weather 137 255" \
+    "time 81 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
+    # "pwd 89 211" \
+    # "mailcount 9 255" \
+    # "now_playing 234 37" \
+    # "cpu 240 136" \
+    # "load 237 167" \
+    # "tmux_mem_cpu_load 234 136" \
+    # "xkb_layout 125 117" \
+    # "date_day 235 136" \
+    # "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+    # "utc_time 230 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+)
 fi
 
