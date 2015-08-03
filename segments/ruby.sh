0721274417f14795474f6c0b261607b424f69f66
tacahiroy <tacahioy@gmail.com>
2015-08-03 17:01:27 +0900

    Add super roughly normal ruby command support

--- segments/ruby.sh
+++ segments/ruby.sh
@@ -4,9 +4,18 @@
 # License     : Modified BSD License
 
 run_segment() {
-  # 2.3.0-dev (set by /home/foo/.rbenv/version)
-  ruby_ver=$(rbenv version | sed 's/ (set by .*)//')
+  if rbenv -v 2>&1 > /dev/null; then
+    # 2.3.0-dev (set by /home/foo/.rbenv/version)
+    ruby_ver=$(rbenv version | sed 's/ (set by .*)//')
+  elif [ -x ruby ]; then
+    # ruby 2.3.0dev (2015-06-01 trunk 50708) [x86_64-darwin14]
+    ruby_ver=$(ruby -v | sed 's/^ruby //' | sed 's/ ([0-9].*)//')
+  else
+    ruby_ver=''
+  fi
 
-	echo "rb:${ruby_ver}"
+  if [ -n "${ruby_ver}" ]; then
+    echo "rb:${ruby_ver}"
+  fi
 	return 0
 }
